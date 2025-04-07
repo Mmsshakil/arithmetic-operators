d9141a014ef1aaee983828f6163cef0b2a35fc79
Mmsshakil <softhandshakil@gmail.com>
2025-04-08 01:08:31 +0600

    if else part

---
+++ if_else.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d", &tk);
+
+    if (tk >= 100)
+    {
+        printf("I will eat burger");
+    }
+    else
+    {
+        printf("I will not eat burger");
+    }
+
+    return 0;
+}
