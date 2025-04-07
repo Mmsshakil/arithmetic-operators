9915d33be1226e2eb240ccfed34e5e06778dedb8
Mmsshakil <softhandshakil@gmail.com>
2025-04-08 02:09:59 +0600

    nased if else added

---
+++ if_else_cox.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d", &tk);
+
+    if (tk >= 5000)
+    {
+        printf("Coxs Bajar jabo\n");
+        if (tk >= 10000)
+        {
+            printf("Saint martin jabo");
+        }
+        else
+        {
+            printf("Saint martin jabo na");
+        }
+    }
+    else
+    {
+        printf("Kothayo jabo na");
+    }
+
+    return 0;
+}
